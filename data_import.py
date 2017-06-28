1cab337ada5da2c2bb061ab1eff209123bf25d3f
nb5hd <nb5hd@virginia.edu>
2017-06-28 17:06:12 -0400

    Added exception handling

--- data_import.py
+++ data_import.py
@@ -1,47 +1,76 @@
 import yaml
+import argparse
 import pandas as pd
 from sqlalchemy import create_engine
 import os
-# TODO More try catch statements for missing files and other errors
 
+# TODO create comprehensive unit tests
+NECESSARY_KEY_SET =  set(["DATA_FILE", "DATABASE", "HOST", "PORT", "TABLE_NAME"])
 
-# TODO use argparser to place config filename as second command line argument 
-# e.g. running the file would be:
-#               python data_import.py <config_file_here> 
-CONFIG_FILENAME = "config.yaml" 
+def parse_filename(parser):
+    parser.add_argument("config_filepath", help="Enter the filepath of the yaml file that is holding the database information")
+    args = parser.parse_args()
+    filepath = args.config_filepath
+    if filepath[-5:]==".yaml":
+        return filepath
+    else:
+        raise Exception("config_filepath must end in '.yaml'")
+
 
 def read_config(file):
     with open(file, 'r') as stream:
         try:
             return yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+        except yaml.YAMLError:
+            raise IOError(file + " does not exist")
+
+
+def check_keys(config_data):
+    config_key_set = set(config_data.keys())
+    if NECESSARY_KEY_SET.issubset(config_key_set):
+        return True
+    else:
+        missing_set = NECESSARY_KEY_SET - config_key_set
+        raise Exception("Missing keywords in config file: " + ', '.join(missing_set))
+
 
 def retrieve_csv(config_data):
-    csv_as_df = pd.read_csv(config_data["DATA_FILE"])
+    try: 
+        csv_as_df = pd.read_csv(config_data["DATA_FILE"])
+    except IOError:
+        raise IOError(config_data["DATA_FILE"] + " does not exist")
     csv_as_df.columns = [col.lower() for col in csv_as_df.columns]
     return csv_as_df
 
 
-# TODO create a virtual envirnonment variable to store db_name, db_user, db_pwd
-def get_database_info():
+def get_database_auth():
     # db credentials are saved in environment variables
-    db_name = os.getenv("DATABASE_NAME")
-    user = os.getenv("DATABASE_USER")
-    return db_name, user
+    db_auth_dict = {}
+    db_auth_dict["DATABASE_NAME"] = str(os.getenv("DATABASE_NAME"))
+    db_auth_dict["DATABASE_USER"] = str(os.getenv("DATABASE_USER"))
+    db_auth_dict["DATABASE_PWD"] = str(os.getenv("DATABASE_PWD"))
+    for k, v in db_auth_dict.items():
+        if v == "None":
+            raise Exception("Enivonmental Variable '" + k + "' does not exist.")  
 
+    return db_auth_dict
 
-def format_db_string(db_name, uname, config_data):
-    return config_data["DATABASE"] + '://' + str(uname) + ':@' + config_data["HOST"] + ':' + str(config_data["PORT"]) + '/' + str(db_name)
+def format_db_string(db_auth_dict, config_data):
+    return config_data["DATABASE"] + '://' + db_auth_dict["DATABASE_USER"] + ':'+ db_auth_dict["DATABASE_PWD"] + '@' + config_data["HOST"] + ':' + str(config_data["PORT"]) + '/' + db_auth_dict["DATABASE_NAME"]
 
 
 def main():
-    config_data = read_config(CONFIG_FILENAME)
+    parser = argparse.ArgumentParser()
+    
+    config_filename = parse_filename(parser) 
+
+    config_data = read_config(config_filename)
+    has_keys = check_keys(config_data)
 
     csv_as_df = retrieve_csv(config_data)
     
-    db_name, uname = get_database_info()
-    db_info = format_db_string(db_name, uname, config_data)
+    db_auth_dict = get_database_auth()
+    db_info = format_db_string(db_auth_dict, config_data)
 
     engine = create_engine(db_info)
 
